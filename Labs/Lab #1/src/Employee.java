88baa6ea06cbce882377c46327ba319c500a0028
UsernameToLon <casey.a.laclair@gmail.com>
2017-09-15 11:28:10 -0400

    All commenting for javadoc on Employee class

--- Labs/Lab #1/src/Employee.java
+++ Labs/Lab #1/src/Employee.java
@@ -1,10 +1,24 @@
+/**
+ * The Employee class is used to hold information for individual employees.
+ * @author Casey LaClair
+ *
+ */
+
 
 public class Employee {
 
-	private String name;
-	private int idNumber;
-	private String department;
-	private String position;
+	private String name;		//employee name
+	private int idNumber;		//employee ID 
+	private String department;  //employees department he/she works in
+	private String position;    //employee position
+	
+	/**
+	 * This constructor sets name, idNumber, department, and position.
+	 * @param name
+	 * @param idNumber
+	 * @param department
+	 * @param position
+	 */
 	
 	public Employee(String name, int idNumber, String department, String position)
 	{
@@ -15,6 +29,14 @@ public class Employee {
 		
 	}
 	
+	
+	/**
+	 * This constructor sets name and idNumber,
+	 * while setting department and position to "".
+	 * @param name
+	 * @param idNumber
+	 */
+	
 	public Employee(String name, int idNumber)
 	{
 		name = this.name;
@@ -23,6 +45,12 @@ public class Employee {
 		position = "";
 		
 	}
+	
+	
+	/**
+	 * This constructor takes no parameters and sets defaults for all employee values.
+	 * "" for Strings and 0 for int.
+	 */
 	
 	public Employee()
 	{
@@ -33,40 +61,88 @@ public class Employee {
 		
 	}
 	
+	
+	/**
+	 * This method sets the employees name from value given.
+	 * @param name
+	 */
+	
 	public void setName(String name)
 	{
 		name = this.name;
 	}
+	
+	
+	/**
+	 * This method sets the employees idNumber from value given.
+	 * @param idNumber
+	 */
 	
 	public void setIdNumber(int idNumber)
 	{
 		idNumber = this.idNumber;
 	}
 	
+	
+	/**
+	 * This method sets the employees department from value given.
+	 * @param department
+	 */
+	
 	public void setDepartment(String department)
 	{
 		department = this.department;
 	}
+	
+	
+	/**
+	 * This method sets the employees position from value given.
+	 * @param position
+	 */
 	
 	public void setPosition(String position)
 	{
 		position = this.position;
 	}
 	
+	
+	/**
+	 * This method gets the name from a employee.
+	 * @return name of employee
+	 */
+	
 	public String getName()
 	{
 		return name;
 	}
+	
+	
+	/**
+	 * This method gets the idNumber from a employee.
+	 * @return idNumber for employee
+	 */
 	
 	public int getIdNumber()
 	{
 		return idNumber;
 	}
 	
+	
+	/**
+	 * This method gets the department an employee works in.
+	 * @return employees department
+	 */
+	
 	public String getDepartment()
 	{
 		return department;
 	}
+	
+	
+	/**
+	 * This method returns the employees position.
+	 * @return employees position
+	 */
 	
 	public String getPosition()
 	{
