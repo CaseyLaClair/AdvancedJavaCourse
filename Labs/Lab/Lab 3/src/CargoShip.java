9609471fbf943dfa3db1ad32df6c651b90e7237b
UsernameToLon <casey.a.laclair@gmail.com>
2017-10-06 10:29:43 -0400

    Completed Lab 3

--- Labs/Lab/Lab 3/src/CargoShip.java
+++ Labs/Lab/Lab 3/src/CargoShip.java
@@ -1,23 +1,60 @@
+/**
+ * This class creates a cargo ship object 
+ * and inherits from the Ship class.
+ */
+public class CargoShip extends Ship
+{
 
-public class CargoShip extends Ship {
-
+	//Declare needed fields
 	private int cargoTonnage;
 	
-	public CargoShip(int cargoTonnage, String name, String year){
+	/**
+	 * This constructor creates a cargo ship object
+	 * @param cargoTonnage - how much cargo
+	 * @param name - name of cargo ship
+	 * @param year - year ship was built
+	 */
+	public CargoShip(int cargoTonnage, String name, String year)
+	{
 		super(name,year);
-		this.cargoTonnage=cargoTonnage;
+		this.cargoTonnage = cargoTonnage;
 	}
 	
-	public int getCargoTonnage() {
+	/**
+	 * This method gets the ships cargo in tons
+	 * @return cargoTonnage
+	 */
+	public int getCargoTonnage() 
+	{
 		return cargoTonnage;
 	}
 	
-	public void setCargoTonnage(int cargoTonnage){
-		this.cargoTonnage=cargoTonnage;
+	/**
+	 * This method gets the cargo ships tonnage
+	 * @param cargoTonnage
+	 */
+	public void setCargoTonnage(int cargoTonnage)
+	{
+		this.cargoTonnage = cargoTonnage;
 	}
 	
-	public String toString() {
-		String str="Ship's name: "+this.getName()+"\nCargo capacity: "+cargoTonnage;
+	/**
+	 * Displays the cargo ships name and cargo tonnage
+	 * @return name and cargoTonnage
+	 */
+	public String toString() 
+	{
+		String str = "Ship's name: " + this.getName() + "\nCargo capacity: " + cargoTonnage;
+		return str;
+	}
+	
+	/**
+	 * Displays all information about the cargo ship
+	 * @return all object information
+	 */
+	public String getSaveState() 
+	{
+		String str = "CargoShip#" + this.getName() + "#" + this.getYear() + "#" + cargoTonnage;
 		return str;
 	}
 }
--- Labs/Lab/Lab 3/src/CruiseShip.java
+++ Labs/Lab/Lab 3/src/CruiseShip.java
@@ -1,23 +1,61 @@
-
-public class CruiseShip extends Ship {
+/**
+ * This class creates a cruise ship object 
+ * and inherits from the Ship class.
+ *
+ */
+public class CruiseShip extends Ship 
+{
 	
+	//Declare all needed fields
 	private int maxPassengers;
 	
-	public CruiseShip(int maxPassengers, String name, String year){
+	/**
+	 * This constructor creates a cruise ship object
+	 * @param maxPassengers - number of people the ship can hold
+	 * @param name - name of the ship
+	 * @param year - year the ship was built
+	 */
+	public CruiseShip(int maxPassengers, String name, String year)
+	{
 		super(name,year);
-		this.maxPassengers=maxPassengers;
+		this.maxPassengers = maxPassengers;
 	}
 	
-	public int getMaxPassengers() {
+	/**
+	 * Gives the max passengers of the ship
+	 * @return maxPassengers
+	 */
+	public int getMaxPassengers() 
+	{
 		return maxPassengers;
 	}
 	
-	public void setMaxPassengers(int maxPassengers){
-		this.maxPassengers=maxPassengers;
+	/**
+	 * Sets the max passengers of the ship
+	 * @param maxPassengers - number of people the ship can hold
+	 */
+	public void setMaxPassengers(int maxPassengers)
+	{
+		this.maxPassengers = maxPassengers;
 	}
 	
-	public String toString() {
-		String str="Ship's name: "+this.getName()+"\nMax passengers: "+maxPassengers;
+	/**
+	 * Displays the cruise ships name and max passengers
+	 * @return name and maxPassengers
+	 */
+	public String toString() 
+	{
+		String str = "Ship's name: " + this.getName() + "\nMax passengers: " + maxPassengers;
+		return str;
+	}
+	
+	/**
+	 * Displays all information about the cruise ship
+	 * @return all object information
+	 */
+	public String getSaveState() 
+	{
+		String str = "CruiseShip#" + this.getName() + "#" + this.getYear() + "#" + maxPassengers;
 		return str;
 	}
 
